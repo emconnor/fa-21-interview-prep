d324f37f0fcd48cd3db3f0ba5050046939855bd6
emconnor <emconnor@seas.upenn.edu>
2021-09-15 10:43:51 -0400

    day 4 finished

--- day4multiplesof3or5/Solution.java
+++ day4multiplesof3or5/Solution.java
@@ -1,12 +1,46 @@
-package day3nextprime;
+package day4multiplesof3or5;
 
 /**
- * Next Prime: given an integer return the next smallest prime number
+ * Multiples of 3 or 5: If we list all the natural numbers below 10 that are
+ * multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000 natural numbers: the
+ * positive integers (whole numbers) 1, 2, 3, etc., and sometimes zero as well.
  */
 
 class Solution {
 
+  public int sumBelow(int n) {
+
+    int sum = 0;
+
+    if (n < 3 && n >= 0) {
+      return 0;
+    }
+
+    if (n < 0) {
+      System.out.println("Enter a natural number.");
+      return -1;
+    }
+
+    for (int i = 3; i < n; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+
+    return sum;
+
+  }
+
   public static void main(String[] args) {
 
+    Solution s = new Solution();
+    System.out.println(s.sumBelow(0));
+    System.out.println(s.sumBelow(3));
+    System.out.println(s.sumBelow(5));
+    System.out.println(s.sumBelow(100));
+    System.out.println(s.sumBelow(20));
+    System.out.println(s.sumBelow(-1));
+    System.out.println(s.sumBelow(1000));
   }
 }
