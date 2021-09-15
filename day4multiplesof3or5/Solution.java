ee8d7953a493fa5504c9c32b10270bac022cad53
emconnor <emconnor@seas.upenn.edu>
2021-09-15 10:32:36 -0400

    day 4 initial commit

---
+++ day4multiplesof3or5/Solution.java
@@ -0,0 +1,12 @@
+package day3nextprime;
+
+/**
+ * Next Prime: given an integer return the next smallest prime number
+ */
+
+class Solution {
+
+  public static void main(String[] args) {
+
+  }
+}
