f8ec7098f80062f78cf034815bf540f31b25abab
emconnor <emconnor@seas.upenn.edu>
2021-09-17 15:44:46 -0400

    done

--- day6romantointeger/Solution.java
+++ day6romantointeger/Solution.java
@@ -1,19 +1,48 @@
 package day6romantointeger;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//import java.util.*;
+
 /**
- * Even Fibonacci numbers: return the sum of all even fibonacci numbers that do
- * not exceed n.
+ * Given a roman numeral, convert it to an integer.
  */
 
 class Solution {
 
   public int romanToInteger(String roman) {
 
-    return -1;
+    Map<Character, Integer> map = new HashMap<>();
+    map.put('M', 1000);
+    map.put('D', 500);
+    map.put('C', 100);
+    map.put('L', 50);
+    map.put('X', 10);
+    map.put('V', 5);
+    map.put('I', 1);
+
+    char[] charArray = roman.toCharArray();
+    int result = 0;
+    int i = 0;
+
+    while (i < charArray.length) {
+      if (charArray.length - i >= 2 && map.get(charArray[i]) < map.get(charArray[i + 1])) {
+        result += map.get(charArray[i + 1]) - map.get(charArray[i]);
+        i += 2;
+      } else {
+        result += map.get(charArray[i]);
+        i++;
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
     Solution s = new Solution();
-
+    System.out.println(s.romanToInteger("XII"));
+    System.out.println(s.romanToInteger("I"));
+    System.out.println(s.romanToInteger("IM"));
+    System.out.println(s.romanToInteger("II"));
   }
 }
