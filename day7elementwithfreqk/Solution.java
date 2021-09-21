34e9c29425b70b6b426cff5f3d2fe61826b0e2c1
emconnor <emconnor@seas.upenn.edu>
2021-09-21 08:12:38 -0400

    initial commit

---
+++ day7elementwithfreqk/Solution.java
@@ -0,0 +1,18 @@
+package day7elementwithfreqk;
+
+
+//import java.util.*;
+
+/**
+ * Find the element that appears k number of times in an array.
+ */
+
+class Solution {
+
+  
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+
+  }
+}
