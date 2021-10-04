52061824fa378ec46a95ed49d839707cb64289ee
emconnor <emconnor@seas.upenn.edu>
2021-10-04 10:22:56 -0400

    made some notes for understanding

--- day17add2nums/Solution.java
+++ day17add2nums/Solution.java
@@ -17,21 +17,33 @@ class Solution {
         return null;
       } 
 
+      // create a dummy head, must initialize to 0 to avoid crashing program because of null exception
+      // in order to not mess up the lists passed to us, we work with copies p and q
       ListNode head = new ListNode(0);
       ListNode p = node1, q = node2, curr = head;
 
-      // initialize x, carry, and result
+      // initialize x, y, carry, and sum
       int x = 0, carry = 0, y = 0, sum = 0;
        
       while (p != null || q != null) {
         
+        // this is a null check, if the node is null, then we have 0
+        // means our program won't crash
         x = (p != null) ? p.val : 0;
         y = (q != null) ? q.val : 0;
+        // here we add x and y and carry
         sum = x + y + carry;
+        // we get the next carry by dividing by 10
         carry = sum / 10;
+        // we make a new ListNode and attach it to current 
+        // sum mod 10 to make sure we get the remainder if the sum is > 10
         curr.next = new ListNode(sum % 10);
+        // move current to curr.next
+        // by creating curr.next before we do this, we avoid the null pointer exception
         curr = curr.next;
+        // do a null check and then set p to p.next
         if (p != null) p = p.next;
+        // do a null check and then set q to q.next
         if (q != null) q = q.next;
       }
 
